186e1d4df8bde3e9c6f0ce614918618d3b44178f
lanhaoxiang <lanhaoxiang@qq.com>
2018-05-23 16:06:46 +0800

    strict code style by eslint

--- examples/DESMerchantClientTest.js
+++ examples/DESMerchantClientTest.js
@@ -1,18 +1,18 @@
-import DESMerchantClient from '../lib/client/DESMerchantClient'
+import DESMerchantClient from "../lib/client/DESMerchantClient";
 
-let client = new DESMerchantClient('5Ka9YjFQtfUUX2Ddnqka...', '1.2.19');
-console.time('data-exchange');
+let client = new DESMerchantClient("5Ka9YjFQtfUUX2Ddnqka...", "1.2.19");
+console.time("data-exchange");
 client.createDataExchangeRequest(4, {
-    name: 'name',
-    idcard: 'idcard'
+    name: "name",
+    idcard: "idcard"
 }).then(result => {
-    console.log('request created:', result);
+    console.log("request created:", result);
     client.getResult(result.request_id).then(result => {
-        console.log(JSON.stringify(result,null,2));
-        console.timeEnd('data-exchange');
+        console.log(JSON.stringify(result, null, 2));
+        console.timeEnd("data-exchange");
     }).catch(ex => {
-        console.error('error when get result:', ex);
+        console.error("error when get result:", ex);
     });
 }).catch(ex => {
-    console.error('error when create data exchange request:', ex);
+    console.error("error when create data exchange request:", ex);
 });
